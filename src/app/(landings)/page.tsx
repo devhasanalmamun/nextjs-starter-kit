4cfd94f46b048efa5b51efb095ff1756ac5efe64
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-05-14 00:56:49 +0600

    enhanced project structure

--- src/app/(landings)/page.tsx
+++ src/app/(landings)/page.tsx
@@ -1,3 +1,26 @@
-export default function Page() {
-  return <div>Homepage</div>;
+import FormInput from "@/components/form/FormInput";
+import { Button } from "@/components/ui/Button";
+import Counter from "@/components/ui/Counter";
+import Heading from "@/components/ui/Heading";
+
+export default function Home() {
+  console.log("Counter import check:", Counter, Counter.Label);
+  return (
+    <div>
+      <Heading variant="h2">Sub Title</Heading>
+      <Button variant="secondary">Submit</Button>
+
+      <form>
+        <FormInput label="Email" type="email" required />
+        <FormInput label="Password" type="password" required />
+      </form>
+
+      <Counter>
+        <Counter.Label>Basic Compound Counter</Counter.Label>
+        <Counter.Count />
+        <Counter.Increase icon="➕" />
+        <Counter.Decrease icon="➖" />
+      </Counter>
+    </div>
+  );
 }
